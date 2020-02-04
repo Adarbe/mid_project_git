3ba983964ded243d8abc769eabc38b26b2c44f40
Adarbe <43495223+Adarbe@users.noreply.github.com>
2020-02-04 13:57:56 +0200

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -14,9 +14,9 @@ node {
       sh "docker images"
       }
     stage('Apply Kubernetes files') {
-    withAWS(region: 'us-east-1', credentials: '<name>') {
+    withAWS(region: 'us-east-1', redentialsId: 'aws-key', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY'') {
     sh """
-    aws eks update-kubeconfig --name <eks name>
+    aws eks update-kubeconfig --name opsSchool-eks-sOuM9kEi
     kubectl apply -f deploy.yml
     """
       }
