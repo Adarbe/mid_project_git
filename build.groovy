1bb238d7be032e02f0fa8ff7be506554449e3b54
Adarbe <43495223+Adarbe@users.noreply.github.com>
2020-02-04 14:43:02 +0200

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -2,22 +2,26 @@ node {
   def customImage = ""
       stage("pull code") {
         checkout scm
+        cleanWs()
        }
-
+    stage('Docker build & push') {
+      app = docker.build( "Credit Card Validator.ipynb:${commitHash}_${BUILD_NUMBER}", " --no-cache ." )
+			app.run("-p 80:8888")
+			curl -L localhost:888
+			app.push()
+			sh(script: "docker rmi ${app.id}")
+			}
       stage("build docker") {
         customImage = docker.build("dockerhub.adarbe")
         withDockerRegistry(credentialsId: 'dockerhub.adarbe') {
         customImage.push()
           }
         }
-    stage("verify dockers") {
-      /bin/sh "docker images"
-      }
     stage('Apply Kubernetes files') {
-    withAWS(region: "us-east-1", redentialsId: "aws-key", usernameVariable: "AWS_ACCESS_KEY_ID", passwordVariable: "AWS_SECRET_ACCESS_KEY") {
-    sh """
-    aws eks update-kubeconfig --name opsSchool-eks-sOuM9kEi
-    kubectl apply -f deploy.yml
+      withAWS(region: "us-east-1", redentialsId: "aws-key", usernameVariable: "AWS_ACCESS_KEY_ID", passwordVariable: "AWS_SECRET_ACCESS_KEY") {
+       sh """
+       aws eks update-kubeconfig --name opsSchool-eks-sOuM9kEi
+       kubectl apply -f deploy.yml
     """
       }
     }
