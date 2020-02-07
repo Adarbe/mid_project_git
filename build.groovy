8ebce90c9a8abf6edeeb70d824fa5c31896e12bd
Adarbe <43495223+Adarbe@users.noreply.github.com>
2020-02-07 16:51:46 +0200

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -6,11 +6,11 @@ node {
       stage('Docker build ') {
 	app = docker.build( "adarbe/card_validation:${BUILD_NUMBER}", " --no-cache ." )
 	withDockerRegistry(credentialsId: 'adarbe') {
-        customImage.push()
+        app.push()
 		}
       }
       stage("build docker") {
-        customImage = docker.build("dockerhub.adarbe")
+        app = docker.build("dockerhub.adarbe")
         withDockerRegistry(credentialsId: 'dockerhub.adarbe') {
           }
         }
