d1b298e18c5a3ee2b8634215d7a7200e2b3fca0a
Adarbe <43495223+Adarbe@users.noreply.github.com>
2020-02-07 16:29:39 +0200

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,18 +1,22 @@
+
+
+
+
+
+
 node {
   def customImage = ""
       stage("pull code") {
         checkout scm
        	}
-      stage('Docker build & push') {
+      stage('Docker build ') {
 	app = docker.build( "adarbe/card_validation:${BUILD_NUMBER}", " --no-cache ." )
 	app.run("-p 80:8888")
-	app.push()
 	sh(script: "docker rmi ${app.id}")
 	}
       stage("build docker") {
         customImage = docker.build("dockerhub.adarbe")
         withDockerRegistry(credentialsId: 'dockerhub.adarbe') {
-        customImage.push()
           }
         }
 }
