6cefd3f0e9a1d2dcfc7e7595475a6d8c4984b61e
Adarbe <43495223+Adarbe@users.noreply.github.com>
2020-02-07 15:57:23 +0200

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -4,7 +4,7 @@ node {
         checkout scm
        	}
       stage('Docker build & push') {
-	app = docker.build( "adarbe/mid_project_git:${BUILD_NUMBER}", " --no-cache ." )
+	app = docker.build( "adarbe/card_validation:${BUILD_NUMBER}", " --no-cache ." )
 	app.run("-p 80:8888")
 	app.push()
 	sh(script: "docker rmi ${app.id}")
