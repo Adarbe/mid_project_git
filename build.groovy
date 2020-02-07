72cbfbae50f5d2e3a12fc553509741c27ffe7e88
Adarbe <43495223+Adarbe@users.noreply.github.com>
2020-02-07 17:38:16 +0200

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,7 +1,7 @@
 node {
   def app = ""
       stage("pull code") {
-	      git url:"https://github.com/Adarbe/card_validation.git"
+	   checkout git "https://github.com/Adarbe/card_validation.git"
       stage('Docker build ') {
 	app = docker.build( "adarbe/card_validation:${BUILD_NUMBER}")
 	withDockerRegistry(credentialsId: 'adarbe') {
