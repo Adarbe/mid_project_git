fc05f0dbbc48124d8c693054f2975722d6b35585
Adarbe <43495223+Adarbe@users.noreply.github.com>
2020-02-08 17:05:05 +0200

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -14,7 +14,7 @@ node {
           }
         }
       stage('Apply Kubernetes files') {
-	withAWS(credentials: 'aws-credentials', region: 'us-east-1') {              	
+	withAWS(region: 'us-east-1', credentials: "jenkins") {              	
 		sh """
 	        aws eks update-kubeconfig --name opsSchool-eks-VT2vjNsB
 	        kubectl apply -f app.ymL
